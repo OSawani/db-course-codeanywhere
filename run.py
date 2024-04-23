0d6a191fd63bcbc83805c17ae2c0ac85ff4e396a
OSawani <DELETED-1699439824325>
2024-04-23 07:21:30 +0000

    second commit

--- run.py
+++ run.py
@@ -1,13 +1,10 @@
-import os # in order to utilise env variables within this file
-from taskmanager import app # app var defined in init file
+import os
+from taskmanager import app
 
-# tell app how and where to run our application
-if __name__ == "__main__": # checking if name class is equal to the main string
+
+if __name__ == "__main__":
     app.run(
         host=os.environ.get("IP"),
         port=int(os.environ.get("PORT")),
         debug=os.environ.get("DEBUG")
     )
-
-
-
--- taskmanager/routes.py
+++ taskmanager/routes.py
@@ -1,8 +1,7 @@
-from flask import render_template # start using flask functionality
-from taskmanager import app, db # from taskmanager package import app and db
+from flask import render_template
+from taskmanager import app, db
 
-# for simpicity to get the app running, we will create a basic app route using the root level directory of / 
-# this will be used to target a function called home which will just return the render template of base.html
+
 @app.route("/")
 def home():
     return render_template("base.html")
