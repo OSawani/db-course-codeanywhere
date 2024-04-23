e43445dae3a9d02aa29774e51aefe1e5f2a8f6be
OSawani <DELETED-1699439824325>
2024-04-23 14:37:03 +0000

    adding tasks

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -1,11 +1,18 @@
 document.addEventListener("DOMContentLoaded", function() {
-  // Sidenav initialization
+  // sidenav initialization
   let sidenav = document.querySelectorAll(".sidenav");
   M.Sidenav.init(sidenav);
 
-  // Modal initialization
-  let modals = document.querySelectorAll('.modal');
-  M.Modal.init(modals);
+  // datepicker initialization
+  let datepicker = document.querySelectorAll(".datepicker");
+  M.Datepicker.init(datepicker, {
+      format: "dd mmmm, yyyy",
+      i18n: {done: "Select"}
+  });
+
+  // select initialization
+  let selects = document.querySelectorAll("select");
+  M.FormSelect.init(selects);
 });
 
   /*
